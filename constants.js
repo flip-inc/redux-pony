15ce0ee1bec37d45c6984878a29599f5fadabc7f
Roger <roger.graham.1393@gmail.com>
2017-01-12 15:19:21 -0500

    Initial commit.

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const DEFAULT_KEY = 'id';
+export const STORE_SI = 'seamless-immutable';
---
+++ index.js
@@ -0,0 +1,5 @@
+export {default as actionCreatorsFor} from './lib/actionCreatorsFor';
+export {default as actionTypesFor} from './lib/actionTypesFor';
+export {default as reducersFor} from './lib/reducersFor';
+export {default as fetchSuccess} from './lib/reducers/fetch/success';
+export { DEFAULT_KEY } from './constants';
